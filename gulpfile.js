30e2c9e7a29fa683834b873cbf7b9001b7311b62
Amio <amio.cn@gmail.com>
2016-09-07 12:12:05 +0800

    Use cssnano instead of gulp-cssnano.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
 const gulp = require('gulp')
 const rename = require('gulp-rename')
 const postcss = require('gulp-postcss')
-const cssnano = require('gulp-cssnano')
 
 gulp.task('css', function () {
   return gulp.src('assets/index.src.css')
     .pipe(rename('index.css'))
     .pipe(postcss([
-      require('autoprefixer')
-      // require('precss')
+      require('autoprefixer'),
+      require('cssnano')
     ]))
-    .pipe(cssnano())
     .pipe(gulp.dest('assets'))
 })
