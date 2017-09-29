a78189daff21c5d58a36762d51f6efb4fb723a47
Amio <amio.cn@gmail.com>
2017-09-29 11:12:01 +0800

    Add 普洛斯金融

--- components/intro-client-startups.js
+++ components/intro-client-startups.js
@@ -19,10 +19,46 @@ const Startup = ({meta}) => {
     <div className='client-card'>
       <img src={'static/clients/' + icon} alt='HiCTO Client' />
       <p>
-        <em>{name} <i class='status'>({funds})</i></em>
+        <em>{name} <i class='status'>{funds && `(${funds})`}</i></em>
         {desc} <br />
         {intro}
       </p>
+      <style jsx>{`
+        .client-card {
+          width: 350px;
+          display: inline-block;
+          margin: 2rem 4rem;
+          text-align: left;
+          vertical-align: top;
+        }
+
+        .client-card img {
+          width: 60px;
+          float: left;
+          margin: 1.2em 0;
+          border-radius: 10px;
+        }
+
+        .client-card em {
+          display: block;
+          font-weight: bold;
+          font-style: normal;
+          margin-bottom: 0.2em;
+        }
+
+        .client-card .status {
+          color: #AAA;
+          font-weight: normal;
+          font-style: normal;
+          /*float: right;*/
+          margin-left: 0.3rem;
+        }
+
+        .client-card p {
+          margin-left: 80px;
+          line-height: 24px;
+        }
+      `}</style>
     </div>
   )
 }
