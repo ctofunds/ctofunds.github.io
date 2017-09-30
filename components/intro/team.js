d21a655a3b178ce273d5bbcf74b0a3c5ccae26f4
Amio <amio.cn@gmail.com>
2017-09-30 12:51:12 +0800

    Sort intro components.

---
+++ components/intro/team.js
@@ -0,0 +1,88 @@
+export default () => {
+  return (
+    <section id='our-team'>
+      <div className='content-wrapper'>
+        <div className='section-title'>关于我们</div>
+        <div className='team-partners'>
+          <div className='partner-card'>
+            <img className='partner-icon' src='static/avatars/icon-gsh.jpg' alt='龚世海' />
+            <p className='partner-title'>创始人 / CEO</p>
+            <p className='partner-name'>龚世海</p>
+            <p className='partner-intro'>
+              上海交通大学硕士，中欧EMBA<br />
+              前大众点评网CTO<br />
+              十年来帮助大众点评建立完整技术体系，从几人扩展到六百人精悍技术研发团队
+            </p>
+          </div>
+        </div>
+        <div className='team-partners'>
+          <div className='partner-card'>
+            <img className='partner-icon' src='static/avatars/icon-pxl.jpg' alt='潘晓良' />
+            <p className='partner-title'>合伙人</p>
+            <p className='partner-name'>潘晓良</p>
+            <p className='partner-intro'>
+              前百姓网CTO，联合创始人<br />
+              十年从零开始打造百姓网“小而美”的技术团队<br />
+              在产品运营，公司文化，人事制度等方面有着丰富的经验
+            </p>
+          </div>
+        </div>
+        <div className='team-partners'>
+          <div className='partner-card'>
+            <img className='partner-icon' src='static/avatars/icon-hx.jpg' alt='何萱' />
+            <p className='partner-title'>投后合伙人</p>
+            <p className='partner-name'>何萱</p>
+            <p className='partner-intro'>
+              前大众点评网人力资源高级经理<br />
+              前1号店人力资源业务合作伙伴<br />
+              全球第二大人力资源公司 Randstad 人力资源解决方案顾问<br />
+              在创业企业的选、用、育、留各环节经验丰富
+            </p>
+          </div>
+        </div>
+        <div className='team-consults'>
+          <div className='team-consults-title'>CTO 顾问</div>
+          <div className='consult-card'>
+            <p className='partner-title'>CTO 顾问</p>
+            <p className='consult-name'>叶亚明</p>
+            <p className='consult-intro'>
+              携程网CTO<br />
+              前eBay高级技术总监<br />
+              2014中国十大最有影响力CTO
+            </p>
+          </div>
+          <div className='consult-card'>
+            <p className='partner-title'>CTO 顾问</p>
+            <p className='consult-name'>韩军</p>
+            <p className='consult-intro'>
+              前1号店CTO<br />
+              前51job网CTO<br />
+              2014中国十大最有影响力CTO
+            </p>
+          </div>
+          <div className='consult-card'>
+            <p className='partner-title'>CTO 顾问</p>
+            <p className='consult-name'>罗剑</p>
+            <p className='consult-intro'>
+              前赶集网 CTO<br />
+              2014中国十大最有影响力CTO
+            </p>
+          </div>
+          <div className='consult-card'>
+            <p className='partner-title'>CTO 顾问</p>
+            <p className='consult-name'>陈烨</p>
+            <p className='consult-intro'>
+              美团大众点评高级副总裁&amp;首席数据官<br />
+              前Microsoft主任科学家&amp;高级研发总监<br />
+              UC Berkeley客座教授<br />
+              2届KDD, 1届SIGIR最佳论文奖
+            </p>
+          </div>
+        </div>
+        <div className='team-pros'>
+          公司同时汇聚一批来自 BAT 等互联网公司资深技术专家，致力于解决企业技术问题。
+        </div>
+      </div>
+    </section>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import PageWrapper from '../components/page-wrapper.js'
 import Header from '../components/header.js'
 import Footer from '../components/footer.js'
-import IntroHero from '../components/intro-hero.js'
-import IntroTeam from '../components/intro-team.js'
-import IntroClientStartups from '../components/intro-client-startups.js'
-import IntroClientDD from '../components/intro-client-dd.js'
+import IntroHero from '../components/intro/hero.js'
+import IntroTeam from '../components/intro/team.js'
+import IntroClientStartups from '../components/intro/client-startups.js'
+import IntroClientDD from '../components/intro/client-dd.js'
 
 export default () => {
   return (
