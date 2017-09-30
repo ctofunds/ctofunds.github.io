803e10ca98028686fb8a54e7932002235413ed91
Amio <amio.cn@gmail.com>
2017-09-30 12:53:09 +0800

    ESLint fix

--- components/page-wrapper.js
+++ components/page-wrapper.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
-import Header from '../components/header.js'
 
-export default ({children}) => {
+export default ({ children }) => {
   return (
     <div className='body-wrapper'>
       <Head>
         <title>HiCTO</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=yes' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, user-scalable=yes'
+        />
         <link rel='stylesheet' href='/static/index.css' media='screen' />
       </Head>
       {children}
