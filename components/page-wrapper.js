b919b0fe836ed0cede9728abc6ebc13c643aab9d
Amio <amio.cn@gmail.com>
2017-12-28 15:40:24 +0800

    Add google site verication

--- components/page-wrapper.js
+++ components/page-wrapper.js
@@ -8,8 +8,10 @@ export default ({ children, className, ...restProps }) => {
         <title>{siteTitle}</title>
         <meta
           name='viewport'
-          content='width=device-width, initial-scale=1, user-scalable=yes'
-        />
+          content='width=device-width, initial-scale=1, user-scalable=yes' />
+        <meta
+          name='google-site-verification'
+          content='pQ9vVY-1bOLgaRtNkiUXhlX49bsWIsLU8y8EIOqslg4' />
         <link rel='stylesheet' href='/static/index.css' media='screen' />
         <meta name='baidu-site-verification' content='ugaSQwguyE' />
       </Head>
