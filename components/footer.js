58b150827e5747a410de760c5ebe1abd835f2bb7
Amio <amio.cn@gmail.com>
2018-01-11 13:08:46 +0800

    Move analytics from footer to document head

--- components/footer.js
+++ components/footer.js
@@ -1,32 +1,6 @@
 import React from 'react'
-import ReactGA from 'react-ga'
-
-// function initGA () {
-//   window.dataLayer = window.dataLayer || []
-//   function gtag () {
-//     window.dataLayer.push(arguments)
-//   }
-//   gtag('js', new Date())
-//
-//   gtag('config', 'UA-111657353-1')
-// }
-
-function initBT () {
-  window._hmt = window._hmt || []
-}
-
-function initGrowingIO () {
-  window._vds = window._vds || []
-  window._vds.push(['setAccountId', '8ea566982832d0c8'])
-}
 
 export default class extends React.Component {
-  componentDidMount () {
-    ReactGA.initialize('UA-111657353-1')
-    // initGA()
-    initBT()
-    initGrowingIO()
-  }
   render () {
     return (
       <footer>
@@ -39,13 +13,6 @@ export default class extends React.Component {
             text-align: center;
           }
         `}</style>
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <script async
-          src='https://www.googletagmanager.com/gtag/js?id=UA-111657353-1' />
-        <script async
-          src='https://hm.baidu.com/hm.js?63148d26d2c8b6f82ac697c9d24b2cd4' />
-        <script async
-          src='https://assets.growingio.com/vds.js' />
       </footer>
     )
   }
