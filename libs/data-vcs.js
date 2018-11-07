065f50249802745734d1963dc12c9940abc64c64
Amio <amio.cn@gmail.com>
2018-11-07 17:04:28 +0800

    add more vc logos

--- libs/data-vcs.js
+++ libs/data-vcs.js
@@ -1,13 +1,22 @@
 export default [
-  { icon: 'static/clients/vcs/jlzb@2x.png' },
+  { icon: 'static/clients/vcs/jwzg@2x.png' },
+  { icon: 'static/clients/vcs/grzb@2x.png' },
+  { icon: 'static/clients/vcs/idg@2x.png' },
+  { icon: 'static/clients/vcs/mfund@2x.png' },
+  { icon: 'static/clients/vcs/ryzg@2x.png' },
+  { icon: 'static/clients/vcs/hhc@2x.png' },
+  { icon: 'static/clients/vcs/zgjj@2x.png' },
   { icon: 'static/clients/vcs/khjj@2x.png' },
+  { icon: 'static/clients/vcs/jlzb@2x.png' },
   { icon: 'static/clients/vcs/dcm@2x.png' },
   { icon: 'static/clients/vcs/ggv@2x.png' },
+  { icon: 'static/clients/vcs/hczb@2x.png' },
   { icon: 'static/clients/vcs/qmct@2x.png' },
   { icon: 'static/clients/vcs/jrzb@2x.png' },
   { icon: 'static/clients/vcs/ygzb@2x.png' },
   { icon: 'static/clients/vcs/zxzb@2x.png' },
-  { icon: 'static/clients/vcs/grzb@2x.png' },
   { icon: 'static/clients/vcs/ttzb@2x.png' },
-  { icon: 'static/clients/vcs/sig@2x.png' }
+  { icon: 'static/clients/vcs/sig@2x.png' },
+  { icon: 'static/clients/vcs/gs@2x.png' },
+  { icon: 'static/clients/vcs/cxgc@2x.png' }
 ]
